8e9ca7018fd8ee2f11bdbfe6febd3b47c5519500
qixiangyang <1801210870@pku.edu.cn>
2018-10-05 17:29:02 +0800

    first commit

---
+++ app/src/main/java/com/example/a12541/weatherforecast/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a12541.weatherforecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by 12541 on 2018/9/26.
+ */
+
+public class MainActivity  extends AppCompatActivity{
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
